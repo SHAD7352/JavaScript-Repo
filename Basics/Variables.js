7aaced4152af4870134f0463b542c4e50760288d
Md Shad Alam <66769654+SHAD7352@users.noreply.github.com>
2024-07-28 09:02:58 +0000

    Variables Commit

---
+++ Basics/Variables.js
@@ -0,0 +1,23 @@
+const EmployeeId = 10;
+
+let EmpName = "Shad"
+
+var EmpGmail = "shad@gmail.com"
+
+PhoneNO = "1234567890"
+
+// EmployeeId = 4  > Not Allowed 
+
+EmpName = "Dilshad"
+EmpName = "dilshad@gmail.com"
+PhoneNO = "25153"
+let Password;
+/*
+Prefer not to use var
+Because of issue in block scope and Funtional scope
+*/
+
+console.log(EmployeeId)
+
+console.table([EmpName, EmpGmail, PhoneNO, Password])
+
